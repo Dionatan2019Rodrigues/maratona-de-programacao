b8e60c342a704359ef49e641246240970b86d5cb
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2024-09-30 16:17:27 -0300

    feat: process and showOutput functions

--- T2-Dionatan/questaoL.cpp
+++ T2-Dionatan/questaoL.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
-void showOutput(string line, int number){
-    cout << line << endl << number << endl;
+void showOutput(string line){
+    cout << line << endl;
 }
+
+string lexicographical(string line, int k){
+    char aux;
+    bool swapped;
+
+    do {
+        swapped = false;
+        for (int i=0;i<line.length()-k;i++) {
+            if (line[i + k] < line[i]) {
+                aux = line[i];
+                line[i] = line[i+k];
+                line[i+k] = aux;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return line;
+}   
 
 int main(){
     int number;
@@ -16,9 +35,8 @@ int main(){
     if(number < 1 || number > line.length() || (line.length()<1) || line.length()> pow(10,5))
         return 0;
 
-    //TODO: process();
-
-    showOutput(line,number);
+    line = lexicographical(line,number);
+    showOutput(line);
 
     return 0;
 }
