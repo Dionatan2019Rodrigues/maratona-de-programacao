4e8261784dca719d6af51cfb5430bf423732a447
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2024-11-05 20:41:06 -0300

    feat: exercise not completed

---
+++ T6-Dionatan/1124.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int L, C, R1, R2;
+    double D, somaDiametros;
+
+    while(cin>>L>>C>>R1>>R2){
+        if(L==0&&C==0&R1==0&&R2==0)
+            continue;
+
+        D = sqrt((L*L)+(C*C));
+        double D1 = R1*2;
+        double D2 = R2*2;
+        somaDiametros = D1+D2;
+        double menorDimensao = (L<C)?L:C; 
+
+        if(somaDiametros <= D){
+            cout << "S" << endl;
+        }else{
+            cout << "N" << endl;
+        }
+    }
+
+    return 0;
+}
