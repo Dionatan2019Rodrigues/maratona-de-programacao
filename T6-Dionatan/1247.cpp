361946ed100456b71bf656564ecbbd83a74a8824
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2024-11-05 20:37:17 -0300

    feat: coast guard exercise

---
+++ T6-Dionatan/1247.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void out(bool possivel_alcancar){
+    possivel_alcancar?cout << "S" << endl:cout << "N" << endl;
+}
+
+int main(){
+
+    double D, VF, VG, DGC, TF, TGC;
+
+    while(cin >> D >> VF >> VG){
+        DGC = sqrt((D*D)+(144));
+
+        TF = 12/VF;
+        TGC = DGC/VG;
+
+        if(TGC<=TF)
+            out(true);
+        else out(false);
+
+    }
+
+    return 0;
