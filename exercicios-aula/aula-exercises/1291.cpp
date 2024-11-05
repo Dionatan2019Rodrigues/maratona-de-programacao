18b3249bf292f3663cd44ab8d1acfe1d79f08f93
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2024-11-05 20:36:23 -0300

    refactor: relocate folder

---
+++ exercicios-aula/aula-exercises/1291.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void out(double one, double two, double three){
+    cout << fixed << setprecision(3) << one << " ";
+    cout << fixed << setprecision(3) << two << " ";
+    cout << fixed << setprecision(3) << three << endl;
+}
+
+int main(){
+
+    double lado;
+
+    while(cin >> lado){
+        
+    }
+
+    return 0;
+}
