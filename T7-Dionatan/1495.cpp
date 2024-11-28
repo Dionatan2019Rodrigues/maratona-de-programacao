d41ba1f139e33a6d1abfaa9fd627245299be0e12
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2024-11-28 01:20:27 -0300

    🐛fix: logic for buy goals and add scores

--- T7-Dionatan/1495.cpp
+++ T7-Dionatan/1495.cpp
@@ -4,45 +4,47 @@
 
 using namespace std;
 
-
 int main() {
-    int N, G;
-    while (cin >> N >> G) {
-        vector<int> diferencaDeGols(N);
-        for (int i = 0; i < N; ++i) {
-            int S, R;
-            cin >> S >> R;
-            diferencaDeGols[i] = R - S; //(negativo se perdeu)
-        }
-        
-        sort(diferencaDeGols.begin(), diferencaDeGols.end());
-        int points = 0;
-        
-//TODO: corrigir lógica de compra de gols e pontuações 
-        for(int i=0;i<diferencaDeGols.size();i++){
-            if(diferencaDeGols[i] == 0){
-                if(G > 0){
-                    G--;
-                    points += 3;
-                }else {
-                    points++;
-                }
+	int N, G, S, R;
 
-            }else if(diferencaDeGols[i] >0){
-                points +=3;
-            }else{
-                int golsNecessarios = (diferencaDeGols[i]*(-1))+1;
-                if(G>= golsNecessarios){
-                    G -= golsNecessarios;
-                    points += 3;
-                }else if(G -1 == golsNecessarios){
-                    G = 0;
-                    points +=1;
-                }
-            }
-        }
-        
-        cout << points << endl;
-    }
-    return 0;
+	while(cin >> N >> G){
+
+		int points = 0;
+		vector<int> golsParaVencer;
+
+		for(int i=0; i<N; ++i){
+			cin >> S >> R;
+
+			if(S > R)
+				points += 3;
+			else
+				golsParaVencer.push_back(R-S+1);
+		}
+
+		sort(golsParaVencer.begin(), golsParaVencer.end());
+
+		for(int goals:golsParaVencer){
+			if(goals == 1){
+				if(G >= 1){
+					G -= 1;
+					points += 3;
+				}else
+					points++;
+			}else{
+				if(G >= goals){
+					G -= goals;
+					points += 3;
+				}else{
+					if(G == goals - 1){
+						G -= (goals - 1);
+						points++;
+					}else
+						break;
+				}
+			}
+
+		}
+		cout << points << endl;
+	}
+
 }
